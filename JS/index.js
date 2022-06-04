b2b0db15db341e2e4b53130c528e32db84cac586
xiaochen <1851133268@qq.com>
2022-06-04 11:54:31 +0800

    6.4

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,10 @@
-var lxfs = document.querySelector('.lxfs')
-var lxfs_btn = document.querySelector('#lxfs_btn')
-var lxfs_flag = 0
-lxfs_btn.onclick = function(){
-    lxfs_flag = !lxfs_flag
-    if(lxfs_flag == 1){
-
-        lxfs.style.display = 'block';
-    }
-    else{
-        lxfs.style.display = 'none'
-    }
+// var lxfs = document.querySelector('.lxfs')
+var lxfs = document.querySelector('#lxfs_btn')
+console.log(lxfs);
+lxfs.onmouseover = function(){
+    console.log(this.children);
+    this.children[1].style.display = 'block'
+}
+lxfs.onmouseout = function(){
+    this.children[1].style.display = 'none'
 }
