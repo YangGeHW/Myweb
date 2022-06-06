1c7adfd38690664f1a9ce694e9125e947090bf2c
xiaochen <1851133268@qq.com>
2022-06-06 19:58:36 +0800

    6.6增加时钟

--- JS/index.js
+++ JS/index.js
@@ -8,3 +8,19 @@ lxfs.onmouseover = function(){
 lxfs.onmouseout = function(){
     this.children[1].style.display = 'none'
 }
+        getTime()
+        setInterval(getTime,1000)
+        function getTime(){
+            
+            var month = document.querySelector('#month')
+            var day = document.querySelector('#day')
+            var hour = document.querySelector('#hour')
+            var minute = document.querySelector('#minute')
+           
+            var time = new Date()
+            month.innerHTML = time.getMonth() + '月'
+            day.innerHTML = time.getDay() + '日'
+            hour.innerHTML = time.getHours() + '时'
+            minute.innerHTML = time.getMinutes() + '分'
+            
+        }
